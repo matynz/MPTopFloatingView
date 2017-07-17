039fedbb710a8dce496e2323869d2795ac0f1ee9
Matias Casanova <maty.casanova@gmail.com>
2017-07-17 17:30:59 -0300

    added app validation

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -2,6 +2,11 @@
 
 echo "Starting post clone of app $BUDDYBUILD_APP_ID"
 
+if [[  $BUDDYBUILD_APP_ID != "59691bd494aa720001181488" ]]; then
+  echo "Skip post clone, not the right app"
+  exit 0
+fi
+
 echo "rm  hooks pre push"
 rm .git/hooks/pre-push
 
